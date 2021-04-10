4060bfa30f01380fb4568d4da860978ec5398b2e
Ruslan Curbanov <rucub100@hhu.de>
2021-04-10 15:11:22 +0200

    init vue app

---
+++ src/app/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Hello World!</h1>
+        <p>
+            We are using Node.js <span id="node-version"></span>, Chromium
+            <span id="chrome-version"></span>, and Electron
+            <span id="electron-version"></span>.
+        </p>
+        <p>
+            {{ message }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            message: "Hello Vue!",
+        };
+    },
+};
+</script>
+
+<style>
+</style>
